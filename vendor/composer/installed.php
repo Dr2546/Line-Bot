dc52fa66466cff37f34c3746b7c39b3bb2f5ba37
Dr2546 <danryv46@gmail.com>
2022-02-09 21:03:20 +0700

    Update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '83ac01622937b30e5ac93debcc4e33d4808dd998',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '83ac01622937b30e5ac93debcc4e33d4808dd998',
+            'dev_requirement' => false,
+        ),
+    ),
+);
